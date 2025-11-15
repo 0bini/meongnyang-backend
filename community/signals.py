37b9c0052b8cda52001385d3f84943492cbd8abc
0bini <sk93264757@gmail.com>
2025-11-16 00:41:05 +0900

    Fix: Change 'content' to 'message' in signals.py for Notification

--- community/signals.py
+++ community/signals.py
@@ -37,7 +37,7 @@ def notify_post_author_on_comment(sender, instance, created, **kwargs):
         # 4. 알림 객체 생성
         Notification.objects.create(
             user=post_author,             # 알림을 받을 사람: 게시글 작성자
-            content=content,              # 알림 내용
+            message=content,              # 알림 내용
             notification_type='comment',  # 알림 종류
             link=link_to                  # 클릭 시 이동할 URL
         )
@@ -67,7 +67,7 @@ def notify_receiver_on_message(sender, instance, created, **kwargs):
         # 4. 알림 객체 생성
         Notification.objects.create(
             user=receiver_user,           # 알림을 받을 사람: 쪽지 수신자
-            content=content,              # 알림 내용
+            message=content,              # 알림 내용
             notification_type='message',  # 알림 종류
             link=link_to                  # 클릭 시 이동할 URL
         )
